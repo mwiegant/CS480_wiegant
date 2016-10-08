14b208127c97e0f69925796c10b7c44f312ed2da
Max Wiegant <max@ustyme.com>
2016-10-08 10:43:47 -0700

    latest

--- PA6/src/object.cpp
+++ PA6/src/object.cpp
@@ -1,77 +1,7 @@
 #include "object.h"
 
 Object::Object(char* objectName)
-{  
-  /*
-    # Blender File for a Cube
-    o Cube
-    v 1.000000 -1.000000 -1.000000
-    v 1.000000 -1.000000 1.000000
-    v -1.000000 -1.000000 1.000000
-    v -1.000000 -1.000000 -1.000000
-    v 1.000000 1.000000 -0.999999
-    v 0.999999 1.000000 1.000001
-    v -1.000000 1.000000 1.000000
-    v -1.000000 1.000000 -1.000000
-    s off
-    f 2 3 4
-    f 8 7 6
-    f 1 5 6
-    f 2 6 7
-    f 7 8 4
-    f 1 4 8
-    f 1 2 4
-    f 5 8 6
-    f 2 1 6
-    f 3 2 7
-    f 3 7 4
-    f 5 1 8
-  */
-
-//  Vertices = {
-//    // {<position>}, {<color>}
-//    {{1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 0.0f}},
-//    {{1.0f, -1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}},
-//    {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}},
-//    {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f, 1.0f}},
-//    {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 0.0f}},
-//    {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}},
-//    {{-1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 1.0f}},
-//    {{-1.0f, 1.0f, -1.0f}, {1.0f, 1.0f, 1.0f}}
-//  };
-//
-//  Indices = {
-//    2, 3, 4,
-//    8, 7, 6,
-//    1, 5, 6,
-//    2, 6, 7,
-//    7, 8, 4,
-//    1, 4, 8,
-//    1, 2, 4,
-//    5, 8, 6,
-//    2, 1, 6,
-//    3, 2, 7,
-//    3, 7, 4,
-//    5, 1, 8
-//  };
-
-//  // The index works at a 0th index
-//  for(unsigned int i = 0; i < Indices.size(); i++)
-//  {
-//    Indices[i] = Indices[i] - 1;
-//  }
-//
-
-/*
-  glGenBuffers(1, &VB);
-  glBindBuffer(GL_ARRAY_BUFFER, VB);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
-//
-  glGenBuffers(1, &IB);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
-*/
-
+{
   // Set object name
   name = objectName;
 
@@ -103,22 +33,18 @@ Object::~Object()
 
 bool Object::Initialize(const char* filePath)
 {
-
-  int numFaces = 0;
-
   Assimp::Importer importer;
   const aiScene *myScene = importer.ReadFile( filePath, aiProcess_Triangulate);
 
   aiMesh* meshOne = myScene->mMeshes[0];
 
-  unsigned int* mIndices = &Indices[0];
-
   glm::vec3 color = glm::vec3(0.5f, 0.2f, 0.0f);
   aiVector3D aiVector;
   unsigned int index;
 
-  // {<position>}, {<color>}
 
+
+  // load the models and the vertices
   for( int i = 0; i < meshOne->mNumFaces; i++ )
   {
     const aiFace& thisFace = meshOne->mFaces[i];
@@ -132,6 +58,8 @@ bool Object::Initialize(const char* filePath)
 
       //get the vertices
       aiVector = meshOne->mVertices[thisFace.mIndices[j]];
+
+      // {<position>}, {<color>}
       Vertex *temp = new Vertex( glm::vec3(aiVector.x, aiVector.y, aiVector.z), color);
       Vertices.push_back( *temp );
 
