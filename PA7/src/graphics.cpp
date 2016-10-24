12aa2fc960fc7d4e9e474f174d00210ca8808b37
Max Wiegant <max@ustyme.com>
2016-10-23 22:11:06 -0700

    changed background color

--- PA7/src/graphics.cpp
+++ PA7/src/graphics.cpp
@@ -150,7 +150,7 @@ void Graphics::Update(unsigned int dt)
 void Graphics::Render()
 {
   //clear the screen
-  glClearColor(0.0, 0.0, 0.2, 1.0);
+  glClearColor(0.1, 0.1, 0.1, 1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   // Start the correct program
