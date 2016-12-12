bc2c5d49981696b75797eadc5a49ade3f2e0bdcc
Jayden Wang <jaydenwang@nevada.unr.edu>
2016-12-12 06:34:09 -0800

    Fragment Shader commit - no gamma, ambient light fixed

--- FinalProject/shaders/fragmentShader.glsl
+++ FinalProject/shaders/fragmentShader.glsl
@@ -8,7 +8,7 @@ out vec4 frag_color;
 
 uniform vec3 eyePosition;
 
-uniform vec3 lightPosition = vec3( 0.0, 20.0, 0.0 );
+uniform vec3 lightPosition = vec3( 0.0, 30.0, 0.0 );
 uniform sampler2D gSampler;
 
 uniform mat4 Model;
@@ -21,19 +21,22 @@ void main()
   vec3 surfaceToLight = normalize( lightPosition - surfacePos );
   vec3 surfaceToCamera = normalize( eyePosition - surfacePos );
 
-  float diffuseCoefficient = max( 0.0, dot(normal, surfaceToLight ) );
-  vec3 diffuse = diffuseCoefficient * surfaceColor.rgb * vec3(1, 1, 1);
+  vec3 intensities = vec3( 1.0, 1.0, 1.0 );
 
-  vec3 ambient = vec3( .1, .1, .1 );
+  float diffuseCoefficient = max( 0.0, dot(normal, surfaceToLight ) );
+  vec3 diffuse = diffuseCoefficient * surfaceColor.rgb * intensities;
+
+  vec3 ambient = float( .3 ) * surfaceColor.rgb * intensities;
 
   float specularCoefficient = 0.0;
   if( diffuseCoefficient > 0.0 )
   {
     specularCoefficient = pow(max(0.0, dot(surfaceToCamera, reflect(-surfaceToLight, normal))), float(.1));
   }
-  vec3 specular = specularCoefficient * vec3(.1, .1, .1) * vec3(.5, .5, .5);
+  vec3 specular = specularCoefficient * vec3(.1, .1, .1) * intensities;
 
   vec3 linearColor = ambient + specular + diffuse;
+
 
   frag_color = vec4( linearColor, 1 );
 } 
